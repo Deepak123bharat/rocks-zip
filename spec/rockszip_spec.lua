e2e026944d17babf293e2faca961d8445d1eeb3d
deepak123bharat <dsrdeepak8@gmail.com>
2021-07-12 18:53:22 +0530

    Added unit test for zip

--- spec/rockszip_spec.lua
+++ spec/rockszip_spec.lua
@@ -1,1 +1,12 @@
---todo
+local zipModule = require("rocks.zip")
+
+describe("Luarocks zip test #unit", function()
+
+    describe("zip.zip", function()
+       it("Compress files in a .zip archive", function()
+          local t, result = zipModule.zip("spec/test")
+          assert.falsy(result)
+          assert.truthy(t)
+       end)
+    end)
+end)
